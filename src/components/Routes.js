317d1a7748fc19433da0966e62e8710a199c3b1c
Andrew Davisson <akdavisson4@gmail.com>
2020-06-25 09:41:42 -0600

    added login view to routes

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Switch, Route } from 'react-router-dom'
 import Home from '../views/Home'
+import Login from '../views/Login'
 
 const Routes = () => {
   return (
@@ -8,6 +9,7 @@ const Routes = () => {
       <Switch>
         // Add routes here
         <Route exact path='/' component={Home} />
+        <Route path='/login' component={Login} />
       </Switch>
     </div>
   )
