ce285d8041fabcfd39580ced91b5c51b955e9167
phil <phil.roth077@gmail.com>
2020-06-25 18:00:34 -0400

    addin login

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 import './App.css'
 import { BrowserRouter } from 'react-router-dom'
 import Routes from './components/Routes'
-import 'fontsource-antic-slab'
+// import 'fontsource-antic-slab'
 
 function App() {
   return (
