14770aa75ae153b319a26023a01a8970d98464a1
phil <phil.roth077@gmail.com>
2020-06-25 18:42:23 -0400

    change img names and start gig ticket eventhandling

--- src/views/GigCardContainer.js
+++ src/views/GigCardContainer.js
@@ -3,34 +3,44 @@ import React, { useState } from "react"
 import { makeStyles, Grid } from "@material-ui/core"
 import GigCard from "./GigCard"
 import { theme } from "../styles/theme"
+import wagSrc from '../assets/Wag.png'
+import taskSrc from '../assets/taskrabbit.png'
+import instaSrc from '../assets/instacart.png'
+import lyftSrc from '../assets/lyft.png'
 
 const gigCardContainerStyle = makeStyles((theme) => ({
   header: {
     fontWeight: "bold",
   },
-  container: {},
+  container: {
+    padding:  20
+  },
 }))
 
 const companyData = [
   {
     id: 1,
     name: "Wag",
-    imgSrc: "../assets/Wag.png",
+    imgSrc: wagSrc,
+    price: 40,
   },
   {
     id: 2,
     name: "Lyft",
-    imgSrc: "../assets/lyft1.png",
+    imgSrc: lyftSrc,
+    price: 20,
   },
   {
     id: 3,
     name: "Taskrabbit",
-    imgSrc: "../assets/TaskrabbitLogo.png",
+    imgSrc: taskSrc,
+    price: 75,
   },
   {
     id: 4,
     name: "Instacart",
-    imgSrc: "../assets/instacart.png",
+    imgSrc: instaSrc,
+    price: 20,
   },
 ]
 
@@ -38,24 +48,17 @@ const GigCardContainer = (props) => {
   // TODO Create test cards
 
   // TODO Will loop through and send props of all gigs down through GigCard
-  const renderAllGigCards = companyData.forEach((gig) => {
-    const { id, name, imgSrc } = gig
-    return <GigCard id={id} name={name} imgSrc={imgSrc} />
+  const renderAllGigCards = companyData.map((gig) => {
+    return <GigCard gigProps={gig} />
   })
 
   const classes = gigCardContainerStyle()
 
-  console.log(renderAllGigCards)
   return (
-    <div className={classes.header}>
+    <div className={classes.container}>
       <h2>Today's Gigs!</h2>
-      <Grid container={true} alignItems='center'>
+      <Grid container={true} alignItems='right' alignContent='right'>
         {renderAllGigCards}
-        <GigCard
-          id={companyData[0].id}
-          name={companyData[0].name}
-          imgSrc={companyData[0].imgSrc}
-        />
       </Grid>
     </div>
   )
