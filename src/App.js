4221f2fcf45e055d05f51b57f0114f3dc1ca9288
Andrew Davisson <akdavisson4@gmail.com>
2020-06-25 21:55:26 -0600

    Wrapped app in ThemeProvider

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from 'react'
 import './App.css'
 import { BrowserRouter } from 'react-router-dom'
+import { ThemeProvider } from '@material-ui/core/styles'
+import { theme } from './styles/theme'
 import Routes from './components/Routes'
 // import 'fontsource-antic-slab'
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes />
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <Routes />
+      </BrowserRouter>
+    </ThemeProvider>
   )
 }
 
